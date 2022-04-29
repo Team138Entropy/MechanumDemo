4212d38f48e50bf1b043989fb033a53e218effd3
GBogonis <george.bogonis@student39.org>
2022-04-29 13:13:30 -0400

    added drive code

--- mecanumDemoDriveCode/src/main/java/frc/robot/Robot.java
+++ mecanumDemoDriveCode/src/main/java/frc/robot/Robot.java
@@ -7,50 +7,83 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.XboxController.Axis;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Jaguar;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
+  //new contoller with code that i totally stole
+  XboxController DriveController = new XboxController(0);
+  //jaguar setup, the channels are just placeholders
+  //front left
+  PWMTalonSRX Talon1 = new PWMTalonSRX(0);
+  //back left
+  PWMTalonSRX Talon2 = new PWMTalonSRX(1);
+  //front right
+  PWMTalonSRX Talon3 = new PWMTalonSRX(2);
+  //back right
+  PWMTalonSRX Talon4 = new PWMTalonSRX(3);
+
+  //mecanum drive object
+  MecanumDrive robotDrive = new MecanumDrive(Talon1, Talon2, Talon3, Talon4);
+
+  //default stuff
+  private SendableChooser<Boolean> mDriveMode;
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+
+  
+
   @Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+    
+    
+    
   }
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {}
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
+  @Override
+  public void robotPeriodic() {
+    updateSmartDashboard();   
+    //setup for the types of mecanum drives
+    if (mDriveMode.getSelected()) {
+      robotDrive.drivePolar(
+       DriveController.getJoystick(XboxController.Side.LEFT,XboxController.Axis.Y),
+       DriveController.getJoystick(XboxController.Side.LEFT,XboxController.Axis.X),
+       DriveController.getJoystick(XboxController.Side.RIGHT,XboxController.Axis.Y));
+    }
+    else {
+      robotDrive.driveCartesian(
+      DriveController.getJoystick(XboxController.Side.LEFT,XboxController.Axis.Y),
+      DriveController.getJoystick(XboxController.Side.LEFT,XboxController.Axis.X),
+      DriveController.getJoystick(XboxController.Side.RIGHT,XboxController.Axis.Y));
+    }
+  }
+
+  //separate function for the dashboard so things don't get messy
+  public void updateSmartDashboard() {
+    //selector for testing the types of drives
+    mDriveMode = new SendableChooser<Boolean>();
+    mDriveMode.setDefaultOption("Polar drive", true);
+    mDriveMode.addOption("Cartesian drive", false);
+    SmartDashboard.putBoolean("on drive polar", mDriveMode.getSelected());
+    SmartDashboard.putNumber("Talon1", Talon1.get());
+    SmartDashboard.putNumber("Talon2", Talon2.get());
+    SmartDashboard.putNumber("Talon3", Talon3.get());
+    SmartDashboard.putNumber("Talon4", Talon4.get());
+    //there is a mecanum drive diagram widget on the shuffleboard so i hope this will make it work.
+    SmartDashboard.putData("mecanum drive", robotDrive);
+  }
+
+
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
@@ -74,33 +107,49 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+
+  }
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+
+  }
 
   /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+
+  }
 
   /** This function is called once when test mode is enabled. */
   @Override
-  public void testInit() {}
+  public void testInit() {
+
+  }
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+
+  }
 
   /** This function is called once when the robot is first started up. */
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+    System.out.println("Simulation is up");
+  }
 
   /** This function is called periodically whilst in simulation. */
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+    System.out.println("simulation is going");
+  }
 }
